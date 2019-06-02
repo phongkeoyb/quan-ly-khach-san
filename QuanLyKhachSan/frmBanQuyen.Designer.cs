64de1df38789189e10b69b1ea043d746eb85eadd
phongkeoyb <51265357+phongkeoyb@users.noreply.github.com>
2019-06-02 17:37:21 +0700

    Sua 1 ti

--- QuanLyKhachSan/frmBanQuyen.Designer.cs
+++ QuanLyKhachSan/frmBanQuyen.Designer.cs
@@ -34,7 +34,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(651, 389);
+            this.ClientSize = new System.Drawing.Size(645, 449);
             this.Name = "frmBanQuyen";
             this.Text = "frmBanQuyen";
             this.ResumeLayout(false);
