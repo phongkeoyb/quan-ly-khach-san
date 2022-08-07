0b2414e0ef0bf5ca852000e75281936ed4e00af9
Phong Le <phonglq@cyfeer.com>
2022-08-08 00:09:58 +0700

    update code

--- QuanLyKhachSan/Common.cs
+++ QuanLyKhachSan/Common.cs
@@ -13,7 +13,7 @@ namespace QuanLyKhachSan
             string cutChar = chuoi.Substring(0, 2);
             string cutNumber = chuoi.Substring(2);
             int number = Int32.Parse(cutNumber);
-            if (number >= 0 && number < 9) return cutChar + "00" + (number + 1);
+            if (number >= 0 && number < 9) return cutChar + (number + 1);
             else if (number >= 9 && number < 99) return cutChar + "0" + (number + 1);
             else return cutChar + (number + 1);
         }
